b0c3de2b0208fb055bd1ae2cc7e20b3ef076f242
JeronimoVargasRebolledo <geromvrebolledo@gmail.com>
2026-02-10 17:57:47 -0500

    Create ejercicio_12.py

---
+++ tarea_poo/ejercicio_12.py
@@ -0,0 +1,16 @@
+# ejercicio 12: salario bruto, retencion y neto
+
+horas = 48
+valor_hora = 5000
+
+# salario bruto
+bruto = horas * valor_hora
+
+# retencion del 12.5%
+retencion = bruto * 0.125
+
+salario_neto = bruto - retencion
+
+print("Salario Bruto:", bruto)
+print("Retencion:", retencion)
+print("Salario Neto:", salario_neto)
